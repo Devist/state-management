d071dfc3a0a2c16dee914f1bf896597677cd13cc
Devist <aboutdh89@naver.com>
2022-03-26 14:35:32 +0900

    feat: redux-toolkit rtk-query - adapt rtk query to component

--- c3_redux-toolkit/p5_rtk-query/components/ArticleList.js
+++ c3_redux-toolkit/p5_rtk-query/components/ArticleList.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
+import articleApi from '../store/article'
+
 const ArticleList = () => {
+  // RTK QUERY 가져오기
+  const {
+    data: articles,
+    isError,
+    isLoading,
+  } = articleApi.useGetArticleListQuery()
+
+  // rtk query 만 사용했을 뿐인데 정말 코드가 많이 줄었다.
   return (
     <div className="container page">
       <div className="row">
         <div className="col-md-9">
-          {status === 'PENDING' && <div>...로딩 중</div>}
+          {isLoading && <div>...로딩 중</div>}
 
-          {status === 'ERROR' && <div>...에러</div>}
+          {isError && <div>...에러</div>}
 
-          {isSuccess &&
+          {!isLoading &&
+            !isError &&
             articles?.map(({ userId, id, title, body }) => (
               <div className="article-preview" key={id}>
                 <div className="preview-link">
--- c3_redux-toolkit/p5_rtk-query/store/article.js
+++ c3_redux-toolkit/p5_rtk-query/store/article.js
@@ -5,7 +5,9 @@ const END_POINT = 'http://localhost:3000'
 const articleApi = createApi({
   // 이 프로젝트, 즉 애플리케이션에 단 하나만 있는 유일값을 넣어야 함
   reducerPath: 'article',
+  // baseQuery 에서는 다양한 옵션을 수용할 수 있음. 여기선 엔드포인트를 넣어줌
   baseQuery: fetchBaseQuery({ baseUrl: END_POINT }),
+  // 엔드포인트 호출 방법
   endpoints: (builder) => ({
     getArticleList: builder.query({
       query: () => '/articles',
